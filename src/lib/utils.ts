3a7595e27fde5ca573d473958daab4bcc8b24d38
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2024-08-22 17:14:13 -0400

    Imported blog posts and added a layout
    - Created Header componenet
    - Made use of Astro Image component
    - Made some helper functions to slugify names/catogories and make dates
    readable

---
+++ src/lib/utils.ts
@@ -0,0 +1,15 @@
+export const slugify = (text: string) => {
+  return text
+    .toLowerCase()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-")
+    .replace(/^-+/, "")
+    .replace(/-+$/, "");
+};
+
+export const formatDate = (date: string) => {
+  return new Date(date).toLocaleDateString("en-US", {
+    timeZone: "UTC",
+  });
+};
